84ba7a3646c7d04777ea45bec794b71dc40d4153
iamallenliu <mrallenliu@gmail.com>
2024-04-12 22:37:39 +0100

    First commit

---
+++ servo2.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+servoPin = 12
+GPIO.setup(servoPin, GPIO.OUT)
+
+pwm = GPIO.PWM(servoPin, 50)  
+
+pwm.start(7)
+
+for i in range(0,180):
+    DC = 1./18.*(i)+2
+    pwm.ChangeDutyCycle(DC)
+    time.sleep(.05)
+    
+for i in range(180,0, -1):
+    DC = 1./18.*(i)+2
+    pwm.ChangeDutyCycle(DC)
+    time.sleep(.05)
+    
+pwm.stop()
+GPIO.cleanup()
